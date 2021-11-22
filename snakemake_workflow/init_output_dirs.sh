a71667510df11d5e6c428c3e2593069b8edff755
Jake Yeung <j.yeung@hubrecht.eu>
2021-11-22 11:10:56 +0100

    Modify init_output_dirs to include positional argument

--- snakemake_workflow/init_output_dirs.sh
+++ snakemake_workflow/init_output_dirs.sh
@@ -4,7 +4,10 @@
 #  
 # 2021-09-12
 
-workdir="/hpc/hub_oudenaarden/jyeung/data/dblchic/simulation_data/snakemake_outputs"
+# workdir="/home/hub_oudenaarden/jyeung/projects/scChIX_ES_NPC/snakemake_pipeline"
+workdir=$1
+[[ ! -d $workdir ]] && echo "$workdir not found, exiting" && exit 1
+[[ ! -d $workdir ]] && mkdir $workdir
 cd $workdir
 
 mkdir scchix_inputs_objs scchix_outputs_objs LDA_outputs_init objs_from_LDA scchix_unmixing_downstream
